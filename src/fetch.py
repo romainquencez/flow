59de934365dc7a6b5d1283d26f3d2758de072379
Romain Quencez <34655643+romainquencez@users.noreply.github.com>
2023-11-29 16:22:59 +0100

    Create fetch.py

---
+++ src/fetch.py
@@ -0,0 +1,22 @@
+import feedparser
+
+feeds = []
+
+feed = feedparser.parse("https://www.lemonde.fr/rss/une.xml")
+
+feeds.append(
+  {
+    "title": feed.get("title"),
+    "description": feed.get("description"),
+    "entries": [
+      {
+        "title": entry.get("title", ""),
+        "link": entry.get("link", ""),
+      } for entry in data.entries
+    ]
+  }
+)
+
+# dump feeds into a JSON file
+with open("feeds.json", "w", encoding="utf-8") as file:
+    json.dump(feeds, file, ensure_ascii=False, indent=4)
