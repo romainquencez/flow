fa43bf4be5e6427f5cb46a7c0e98570a853c9f2e
Romain Quencez <34655643+romainquencez@users.noreply.github.com>
2023-11-29 16:53:57 +0100

    Update fetch.py

--- src/fetch.py
+++ src/fetch.py
@@ -3,21 +3,24 @@ import json
 
 feeds = []
 
-feed = feedparser.parse("https://www.lemonde.fr/rss/une.xml")
+# read feeds URLs from txt file
+txt_file = open("feeds.txt", "r")
 
-feeds.append(
-  {
-    "title": feed.get("title"),
-    "link": feed.get("link"),
-    "description": feed.get("description"),
-    "entries": [
-      {
-        "title": entry.get("title", ""),
-        "link": entry.get("link", ""),
-      } for entry in feed.entries
-    ]
-  }
-)
+for line in txt_file.readlines():
+  feed = feedparser.parse(line)
+  feeds.append(
+    {
+      "title": feed.get("title"),
+      "link": feed.get("link"),
+      "description": feed.get("description"),
+      "entries": [
+        {
+          "title": entry.get("title", ""),
+          "link": entry.get("link", ""),
+        } for entry in feed.entries
+      ]
+    }
+  )
 
 # dump feeds into a JSON file
 with open("_data/feeds.json", "w", encoding="utf-8") as file:
