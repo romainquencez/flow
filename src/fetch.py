7e6e61769be53099658026e40016c2e9ab44fb10
Romain Quencez <34655643+romainquencez@users.noreply.github.com>
2023-11-29 20:51:45 +0100

    Update fetch.py

--- src/fetch.py
+++ src/fetch.py
@@ -2,28 +2,36 @@ import feedparser
 import json
 
 feeds = []
+entries = []
 
 # read feeds URLs from txt file
 txt_file = open("feeds.txt", "r")
 
 for line in txt_file.readlines():
   data = feedparser.parse(line)
+  entries.append(
+    {
+        "title": entry.title,
+        "link": entry.link,
+        "description": entry.description,
+        "date": entry.get("published", entry.get("created", entry.get("updated"))),
+        "image": None,
+        "feed": {
+          "title": data.feed.title,
+        }
+      } for entry in data.entries
+  )
   feeds.append(
     {
       "title": data.feed.title,
       "link": data.feed.link,
-      "entries": [
-        {
-          "title": entry.title,
-          "link": entry.link,
-          "description": entry.description,
-          "date": entry.get("published", entry.get("created", entry.get("updated"))),
-          "image": None,
-        } for entry in data.entries
-      ]
     }
   )
 
 # dump feeds into a JSON file
 with open("_data/feeds.json", "w", encoding="utf-8") as file:
     json.dump(feeds, file, ensure_ascii=False, indent=4)
+
+# dump entries into a JSON file
+with open("_data/entries.json", "w", encoding="utf-8") as file:
+    json.dump(entries, file, ensure_ascii=False, indent=4)
