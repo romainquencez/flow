22b3b1653cda197533b8dba8ad2a46e7ef317ee1
Romain Quencez <34655643+romainquencez@users.noreply.github.com>
2023-12-01 11:37:28 +0100

    Update fetch.py

--- src/fetch.py
+++ src/fetch.py
@@ -7,65 +7,78 @@ from slugify import slugify
 
 feeds = []
 entries = []
+groups = []
 
 # read feeds URLs from txt file
-txt_file = open("feeds.txt", "r")
+data = json.load(open("feeds.json", "r"))
 
-for line in txt_file.readlines():
-    try:
-        data = feedparser.parse(line)
-        slug = slugify(data.feed.title)
-        url = f"{slug}.html"
-    
-        feed_entries = []
-    
-        # add entries
-        for entry in data.entries:
-            # get publication date
-            published_on = (
-                entry.get("published")
-                or entry.get("created")
-                or entry.get("updated")
-            )
-            published_on_parsed = None
-            if published_on is not None:
-                try:
-                    published_on_parsed = parser.parse(published_on, fuzzy=True)
-                except (TypeError, parser.ParserError):
-                    pass
+# iterate on groups
+for group in data.keys():
+    group_slug = slugify(group)
+    groups.append(
+        {
+            "title": group,
+            "slug": group_slug,
+        }
+    )
+
+    # iterate on group's feeds
+    for feed in data[group]:
+        try:
+            data = feedparser.parse(line)
+            slug = slugify(data.feed.title)
+            url = f"{slug}.html"
         
-            feed_entry = {
-                "title": entry.title,
-                "link": entry.link,
-                "description": entry.description,
-                "date": published_on_parsed,
-                "image": entry.enclosures[0].href if len(entry.enclosures) else None,
-                "feed": {
-                  "title": data.feed.title,
-                },
-            }
-            entries.append(feed_entry)
-            feed_entries.append(feed_entry)
-
-        # add feed
-        feeds.append(
-            {
-                "title": data.feed.title,
-                "link": data.feed.link,
-                "slug": slug,
-                "filename": url,
-                "description": data.feed.get("subtitle"),
-                "image": data.feed.get("logo") or (data.feed.image["href"] if data.feed.get("image") else None),
-                "entries": feed_entries,
-            }
-        )
+            feed_entries = []
+        
+            # add entries
+            for entry in data.entries:
+                # get publication date
+                published_on = (
+                    entry.get("published")
+                    or entry.get("created")
+                    or entry.get("updated")
+                )
+                published_on_parsed = None
+                if published_on is not None:
+                    try:
+                        published_on_parsed = parser.parse(published_on, fuzzy=True)
+                    except (TypeError, parser.ParserError):
+                        pass
+            
+                feed_entry = {
+                    "title": entry.title,
+                    "link": entry.link,
+                    "description": entry.description,
+                    "date": published_on_parsed,
+                    "image": entry.enclosures[0].href if len(entry.enclosures) else None,
+                    "feed": {
+                      "title": data.feed.title,
+                    },
+                }
+                entries.append(feed_entry)
+                feed_entries.append(feed_entry)
     
-        # create page for feed
-        shutil.copyfile("sample-feed.html", url)
-    except Exception as error:
-        print(line)
-        print(error)
-        pass
+            # add feed
+            feeds.append(
+                {
+                    "title": data.feed.title,
+                    "link": data.feed.link,
+                    "slug": slug,
+                    "filename": url,
+                    "description": data.feed.get("subtitle"),
+                    "image": data.feed.get("logo") or (data.feed.image["href"] if data.feed.get("image") else None),
+                    "entries": feed_entries,
+                    "group": group_slug,
+                }
+            )
+        
+            # create page for feed
+            shutil.copyfile("sample-feed.html", url)
+        except Exception as error:
+            print(line)
+            print(error)
+            pass
 
 # sort all entries by date
 entries = sorted(entries, key=lambda d: d["date"], reverse=True) 
@@ -80,3 +93,7 @@ with open("_data/feeds.json", "w", encoding="utf-8") as file:
 # dump entries into a JSON file
 with open("_data/entries.json", "w", encoding="utf-8") as file:
     json.dump(entries, file, ensure_ascii=False, indent=4, default=str)
+
+# dump groups into a JSON file
+with open("_data/groups.json", "w", encoding="utf-8") as file:
+    json.dump(groups, file, ensure_ascii=False, indent=4, default=str)
